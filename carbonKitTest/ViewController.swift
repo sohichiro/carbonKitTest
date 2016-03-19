042263c102b97023e39da956270599d548f5535b
sohichiro <urochiiuso_ogana_@i.softbank.jp>
2016-03-19 12:08:13 +0900

    Initial Commit

---
+++ carbonKitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  carbonKitTest
+//
+//  Created by 長尾 聡一郎 on 2016/03/19.
+//  Copyright © 2016年 長尾 聡一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
